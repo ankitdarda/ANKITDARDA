385ca96a87455fce5e75c87f212771d93750ca9f
ankitdarda <ankitdarda@gmail.com>
2018-09-07 22:21:34 -0500

    Revert "initial generated site"
    This reverts commit 450538412eabb1cccf4785215548a97e55650e05.

--- wp-content/uploads/2015/12/index.php
+++
@@ -1,3 +0,0 @@
-<br /><br />
-<center>Sorry, you are not authorized to access this area ! Goodbye !</center><br />
-<center>Protect your online content with <a href="http://yooplugins.com/downloads/wp-content-copy-protection-pro/">WP Content Copy Protection Pro</a> - The No.1 WordPress Copy Protection Plugin on the Web</center><br />
